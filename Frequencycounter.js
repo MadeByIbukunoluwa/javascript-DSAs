02bec80a4aa4b78c04ae3691ca3a94d6692e6fb6
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-12-30 01:02:59 +0100

    added frequency counter

---
+++ Frequencycounter.js
@@ -0,0 +1,57 @@
+
+// Write a function same which accepts two arrays . The function should return true if every value in the aray has it's corresponding value squared in the second array 
+
+// My first solution 
+
+
+function same (arr1,arr2) {
+    if (arr1.length !== arr2.length) {
+        return false 
+    }
+    arr1 =  arr1.sort((a, b) => a - b)
+    arr2 = arr2.sort((a, b) => a - b)
+   return arr1.every((el, index) => el ** 2 === arr2[index] );
+}
+
+console.log(same([1,2,3,2],[9,1,4,4]))
+
+
+// naive solution presented
+
+function same1 (arr1,arr2) {
+    for (let i = 0 ; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if (correctIndex === -1) {
+            return false 
+        }
+    }
+    return true 
+}
+
+console.log(same1([1, 2, 3, 2], [9, 1, 4, 4]));
+
+
+function same2 (arr1,arr2) {
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for (let val of arr1 ) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1 
+    }
+    for (let val of arr2 ) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1 
+    }
+    for (let key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false 
+        }
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false 
+        }
+    }
+    return true 
+}
+
+
+console.log(same2([1, 2, 3, 2], [9, 1, 4, 4]));
+
+
