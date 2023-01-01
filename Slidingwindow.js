0258dda5392f079766794ae9325814e2626223e2
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-01-01 03:05:01 +0100

    modified slidingwindow files

--- Slidingwindow.js
+++ Slidingwindow.js
@@ -1,1 +1,25 @@
 "use strict";
+// Write a function called maxSubArraySum which accepts an array of integers and a number called n . The function should calculate the maximum sum of n consecutive elements in the array. 
+//naive implementation
+function maxSubArraySum(arr, num) {
+    let max = -Infinity;
+    // If we have an array that are all negative numbers , our biggest sum will still be negative
+    if (arr.length === 0 || num > arr.length)
+        return null;
+    for (let i = 0; i < arr.length - num + 1; i++) {
+        // we make te loop this hort so that that can be the last place we will start our sum 
+        let temp = 0;
+        for (let j = 0; j < num; j++) {
+            temp += arr[i + j];
+        }
+        if (temp > max) {
+            max = temp;
+        }
+    }
+    return max;
+}
+console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5,], 2));
+console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5, 7, 9, 0, 4, 8, 5, 7, 6, 4, 3, 2, 2, 2, 2, 2, 6, 6, 4, 2, 2, 2, 2, 4, 5, 3, 21,], 2));
+console.log(maxSubArraySum([4, 2, 1, 6], 4));
+console.log(maxSubArraySum([4, 2, 1, 6, 2], 1));
+console.log(maxSubArraySum([], 4));
