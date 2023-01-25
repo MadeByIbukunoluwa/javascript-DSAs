43cf70d8e0f2909bf97d4ecd68e1ea68df893b54
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-01-25 20:13:49 +0100

    added hashtables

--- DataStructures/BinarySearchTree/BinarySearchTree.js
+++ DataStructures/BinarySearchTree/BinarySearchTree.js
@@ -76,6 +76,51 @@ class BST {
         }
         return false
     }
+    BFS() {
+        let data = []
+        let queue = []
+        let node = this.root
+
+        queue.push(node)
+        while (queue.length) {
+            node = queue.shift()
+            data.push(node)
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        return data 
+    }
+    DFSPreOrder() {
+        let data = []
+        function traverse (node) {
+                data.push(node.value)
+                if (node.left) traverse(node.left)
+                if(node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data 
+    }
+    DFSPostOrder() {
+        let data = []
+        function traverse (node) {
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        data.push(node.value)
+     }
+        traverse(this.root)
+        return data 
+    }
+    DFSInOrder() {
+         let data = [];
+         function traverse(node) {
+           (node.left) && traverse(node.left);
+           // You can use the and function but use if statement to make it more verbose 
+           data.push(node.value);
+           (node.right) && traverse(node.right);
+         }
+         traverse(this.root);
+         return data; 
+    }
 }
 
 let tree = new BST()
@@ -86,7 +131,7 @@ let tree = new BST()
 // tree.root.left = new node(7)
 // tree.root.left.right = new node(9)
 // tree.root.left.left = new node(5)
-tree.insert(10)
+tree.insert(10)                  
 tree.insert(5)
 tree.insert(13)
 tree.insert(11)
@@ -95,3 +140,7 @@ tree.insert(16)
 tree.insert(7)
 console.log(tree.find(16))
 console.log(tree)
+console.log(tree.BFS())
+console.log(tree.DFSPreOrder())
+console.log(tree.DFSPostOrder())
+console.log(tree.DFSInOrder())
